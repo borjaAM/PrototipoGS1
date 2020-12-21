f1b587665a55cf1aa316212552d49efc64a2f581
NicolasReyAlonso <NicolasReyAlonso@gmail.com>
2020-12-21 21:53:37 +0000

    unidos

--- app/src/main/java/es/ulpgc/gs1/view/userView/MainMenuActivity.java
+++ app/src/main/java/es/ulpgc/gs1/view/userView/MainMenuActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 
 import es.ulpgc.gs1.R;
+import es.ulpgc.gs1.view.Calendar;
 import es.ulpgc.gs1.view.loginAndregister.MainActivity;
 import es.ulpgc.gs1.view.patient.ModifyTreatmentPlanActivity;
 import es.ulpgc.gs1.view.patient.PatientListActivity;
@@ -37,6 +38,10 @@ public class MainMenuActivity extends AppCompatActivity {
 
     public void go_to_my_profile(View view){
         Intent i = new Intent(this, MyProfileActivity.class);
+        startActivity(i);
+    }
+    public void go_to_modify_calendar(View view){
+        Intent i = new Intent(this, Calendar.class);
         startActivity(i);
     }
 
